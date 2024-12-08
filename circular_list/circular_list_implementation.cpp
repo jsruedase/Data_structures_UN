d583227e6a3cd902cd5f0d871bf7f6d993c0ceaa
Juan Sebastián Rueda Segura <159072764+jsruedase@users.noreply.github.com>
2024-12-08 17:11:07 -0500

    checked circular_list_implementation.cpp

--- circular_list/circular_list_implementation.cpp
+++ circular_list/circular_list_implementation.cpp
@@ -1,106 +1,101 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> class CircularList {
+template <typename T>
+class CircularList {
     int front = -1;
     int rear = -1;
     int size;
     T* arr;
 
-    CircularList(int n){
+public:
+    CircularList(int n) {
         size = n;
         arr = new T[n];
     }
-    ~CircularList(){
+
+    ~CircularList() {
         delete[] arr;
     }
 
-    void pushBack(T data){
-
-        if ((rear + 1) % size == front){
+    void pushBack(T data) {
+        if ((rear + 1) % size == front) {
             cout << "Error: Lista llena. No se puede agregar el elemento.\n";
-        }
-
-        else if (front == -1){
+        } else if (front == -1) {
             front = 0;
             rear = 0;
             arr[rear] = data;
-        }       
-
-        else{
+        } else {
             rear = (rear + 1) % size;
             arr[rear] = data;
         }
     }
 
-    void pushFront(T data){
-        if ((rear + 1) % size == front){
+    void pushFront(T data) {
+        if ((rear + 1) % size == front) {
             cout << "Error: Lista llena. No se puede agregar el elemento.\n";
-        }
-
-        else if (front == -1){
+        } else if (front == -1) {
             front = 0;
             rear = 0;
             arr[rear] = data;
-        }       
-
-        else{
-            front = (front + 1) % size;
+        } else {
+            front = (front - 1 + size) % size;  // Decremento circular
             arr[front] = data;
         }
     }
 
-    void popBack(){
-        if (front == -1){
-            cout << "Error: Lista vacia. No se puede eliminar el elemento.\n";
-        }
-
-        else if (front == rear){
+    void popBack() {
+        if (front == -1) {
+            cout << "Error: Lista vacía. No se puede eliminar el elemento.\n";
+        } else if (front == rear) {
             front = -1;
             rear = -1;
-        }       
-
-        else{
-            rear = (rear-1) % size;
+        } else {
+            rear = (rear - 1 + size) % size;  // Decremento circular
         }
     }
 
-    void popFront(){
-        if (front == -1){
-            cout << "Error: Lista vacia. No se puede eliminar el elemento.\n";
-        }
-
-        else if (front == rear){
+    void popFront() {
+        if (front == -1) {
+            cout << "Error: Lista vacía. No se puede eliminar el elemento.\n";
+        } else if (front == rear) {
             front = -1;
             rear = -1;
-        }       
-
-        else{
-            front = (front + 1) % size;
+        } else {
+            front = (front + 1) % size;  // Incremento circular
         }
     }
 
-    void printf(){
-        if (rear >= front){
-            cout << "Elementos en el arreglo: ["
-
-            for (int i = front; i < rear + 1, i++){
-                if (i != rear) cout << arr[i] <<" ";
-                else cout << arr[i] <<"";
-            }
-            cout << "]";
+    void print() {
+        if (front == -1) {
+            cout << "La lista está vacía.\n";
+            return;
         }
 
-        else {
-            cout <<"Elementos en el arreglo: [";
-
-            for (int i = front;  i < size, i++){
-                if (i != rear) cout << arr[i] <<" ";
-            }
-            for (int j = 0;  i < rear + 1, i++){
-                if (i != rear) cout << arr[i] <<" ";
-                else cout << arr[i] <<"";
-            }
-            cout << "]";
+        cout << "Elementos en el arreglo: [";
+        int i = front;
+        while (true) {
+            cout << arr[i];
+            if (i == rear) break;
+            cout << " ";
+            i = (i + 1) % size;
         }
+        cout << "]\n";
+    }
 };
+
+int main() {
+    CircularList<int>* lst = new CircularList<int>(3);
+    lst->pushBack(1);
+    lst->pushBack(2);
+    lst->print();  // [1 2]
+    lst->pushBack(3);  // Error: Lista llena
+    lst->popFront();
+    lst->print();  // [2]
+    lst->pushBack(3);
+    lst->print();  // [2 3]
+    lst->pushFront(0);
+    lst->print();  // [0 2 3]
+    delete lst;
+    return 0;
+}
